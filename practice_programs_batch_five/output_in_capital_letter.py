b687a69ce9561eb79c3d3ebd8730abb69041644c
nolaram <marloncopino10@gmail.com>
2025-03-20 15:51:02 +0800

    print_output_in_capital_letter

--- practice_programs_batch_five/output_in_capital_letter.py
+++ practice_programs_batch_five/output_in_capital_letter.py
@@ -1,4 +1,6 @@
 # ask the user to input its full name
-user_name = str(input("Enter your fullname: "))
+user_name = str(input("\nEnter your fullname: "))
 
 # print the output in capital letter
+capital_name = user_name.upper()
+print(capital_name)
