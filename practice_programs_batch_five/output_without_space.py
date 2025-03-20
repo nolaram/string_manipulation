b786a6b7a465c1799d8ba8403eca0e9a4ddbb2e6
nolaram <marloncopino10@gmail.com>
2025-03-20 15:30:41 +0800

    print_output_without_space

--- practice_programs_batch_five/output_without_space.py
+++ practice_programs_batch_five/output_without_space.py
@@ -1,4 +1,6 @@
 # ask user to input name with spaces in the beginning 
-user_name = str(input("Enter your fullname: "))
+user_name = str(input("\nEnter your fullname: "))
 
 # print the output without the spaces
+remove_space = user_name.strip()
+print (remove_space)
