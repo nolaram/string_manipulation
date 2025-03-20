e4653b23c07ff7266d9c4586b24f10524e38b6d7
nolaram <marloncopino10@gmail.com>
2025-03-20 15:43:18 +0800

    fill_with_zero_till_six_digits

--- practice_programs_batch_five/fill_input_with_zero_till_six_digits.py
+++ practice_programs_batch_five/fill_input_with_zero_till_six_digits.py
@@ -1,4 +1,6 @@
 # ask the user to input a number (0 - 1000)
-number = int(input("Enter a number from 0 to 1000: "))
+number = input("\nEnter a number from 0 to 1000: ")
 
 # print the output with zero's that fill it till it's six digits
+six_digit = number.zfill(6)
+print(six_digit)
