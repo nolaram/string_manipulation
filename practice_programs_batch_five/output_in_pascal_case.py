0a2a17d7f4dbbecc4a40c906f098ed31eeed7d29
nolaram <marloncopino10@gmail.com>
2025-03-20 16:42:04 +0800

    fixed_errors

--- practice_programs_batch_five/output_in_pascal_case.py
+++ practice_programs_batch_five/output_in_pascal_case.py
@@ -5,7 +5,7 @@ user_name = str(input("\nEnter your full name: "))
 remove_space = user_name.strip()
 
 # fix the casing
-pascal_case = remove_space.title()
+pascal_case = remove_space.title().replace(" ","")
 
 # print the output
 print(pascal_case)
