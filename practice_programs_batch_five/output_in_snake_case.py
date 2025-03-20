a7f4b61ac399e6a41e221a1235bb9ca077ee1ad9
nolaram <marloncopino10@gmail.com>
2025-03-20 16:48:38 +0800

    input_name

--- practice_programs_batch_five/output_in_snake_case.py
+++ practice_programs_batch_five/output_in_snake_case.py
@@ -1,3 +1,5 @@
 # ask user to input fullname in incorrect casing
+user_name = str(input("Enter your fullname: "))
+
 # convert the casing to lower case and replace the space with an underscore
 # print the output
