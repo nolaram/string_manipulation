f756cd375ca91070ff89aa96257b2a5d729e2c59
nolaram <marloncopino10@gmail.com>
2025-03-20 16:27:21 +0800

    print_output

--- practice_programs_batch_five/number_of_characters.py
+++ practice_programs_batch_five/number_of_characters.py
@@ -1,7 +1,8 @@
 # ask user for its fullname
-user_name = str(input("Enter your fullname: "))
+user_name = str(input("\nEnter your fullname: "))
 
 # count the number of characters in the name
 character_count = len(user_name)
 
 # print the output
+print(character_count)
