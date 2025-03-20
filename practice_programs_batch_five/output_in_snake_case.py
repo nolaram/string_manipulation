a9784f3f0446051f66ae39eb85bf342cdc1767d5
nolaram <marloncopino10@gmail.com>
2025-03-20 16:50:20 +0800

    convert_to_snake_case

--- practice_programs_batch_five/output_in_snake_case.py
+++ practice_programs_batch_five/output_in_snake_case.py
@@ -2,4 +2,6 @@
 user_name = str(input("Enter your fullname: "))
 
 # convert the casing to lower case and replace the space with an underscore
+snake_case = user_name.lower().replace(" ","_")
+
 # print the output
